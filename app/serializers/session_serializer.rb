7c12ffbc47f310689290991938af7994419f3e58
Ryan McDonald <rjmcdonald83@gmail.com>
2014-06-16 13:56:09 -0500

    Ensure Session serializer has an id and accurate description per the Proposal

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -1,4 +1,11 @@
 class SessionSerializer < ActiveModel::Serializer
-  attributes :conference_day, :start_time, :end_time, :title, :presenter,
-    :room_name, :track_name
+  attributes :conference_day, :start_time, :end_time, :title, :presenter, :room_name, :track_name, :id, :description
+
+  def description
+    if object.proposal
+      object.proposal.abstract
+    else
+      object.description
+    end
+  end
 end

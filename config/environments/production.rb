9a4b45c31b2a254e08f30da5cee9efbe5a0712a7
Marty Haught <mghaught@gmail.com>
2014-08-20 20:12:09 -0600

    Swapped out example email with rubycentral one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ CFPApp::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -79,7 +79,7 @@ CFPApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
-  config.action_mailer.default_options = {from: 'cfp@example.org'}
+  config.action_mailer.default_options = {from: 'rubyconf@rubycentral.org'}
 
   config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',

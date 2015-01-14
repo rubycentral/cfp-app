b72460eb440732bdd6e4bf5c28df4ae7f288fafc
Ashley <a.clarkem@gmail.com>
2015-01-14 16:41:32 -0700

    reformatted dates so they are clearer

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,3 @@
-Time::DATE_FORMATS[:month_day_year] = "%a, %b %d %Y"
-Time::DATE_FORMATS[:long_with_zone] = "%B %-d, %Y at %I:%M%P %Z"
+Time::DATE_FORMATS[:month_day_year] = "%b %d, %Y"
+Time::DATE_FORMATS[:long_with_zone] = "%b %-d, %Y at %I:%M%P %Z"
+Time::DATE_FORMATS[:day_at_time] = "%-d %b @ %H:%M"

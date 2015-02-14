9dd11c0e7be5e54a275a0991967798f1b7621135
Marty Haught <mghaught@gmail.com>
2015-02-13 21:14:20 -0700

    Updated to the datetimepicker widget

--- app/assets/javascripts/admin/events.js
+++ app/assets/javascripts/admin/events.js
@@ -1,8 +1,5 @@
 $(document).ready(function () {
-  $('#event_closes_at, #event_opens_at, #event_start_date, #event_end_date').datetimepicker({
-    controlType: 'select',
-    dateFormat: 'M d, y at'
-  });
+  $('#event_closes_at, #event_opens_at, #event_start_date, #event_end_date').datetimepicker();
 
   $('.dataTables_info').addClass('text-muted');
 });

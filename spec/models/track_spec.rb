2ebf2ab4c32cef3c6bd17214e51d96d835973efa
Marty Haught <mghaught@gmail.com>
2015-02-24 13:22:19 -0700

    Scoped track uniqueness constraint by event

--- spec/models/track_spec.rb
+++ spec/models/track_spec.rb
@@ -9,6 +9,17 @@ describe Track do
       expect(track2).to be_invalid
     end
 
+    it "allows a duplicate name from another event" do
+      old_event = create(:event, name: "Old")
+      new_event = create(:event, name: "New")
+
+      original_track = old_event.tracks.create(name: "Dup")
+      expect(original_track).to be_valid
+
+      dup_track = new_event.tracks.build(name: "Dup")
+      expect(dup_track).to be_valid
+    end
+
     it "requires that name is present" do
       track = build(:track, name: nil)
       expect(track).to be_invalid

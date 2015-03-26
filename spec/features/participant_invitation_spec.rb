f630317ea3baeb4dd02d4bb25cd2a96e58e2d21b
Kelly Pederson <Kelly.Pederson@gmail.com>
2015-03-26 09:31:25 -0700

    Combined accepted inviations and participants in to one table

--- spec/features/participant_invitation_spec.rb
+++ spec/features/participant_invitation_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 feature 'Participant Invitations' do
   let(:user) { create(:person) }
   let(:invitation) { create(:participant_invitation, role: 'organizer') }
+  let(:event) { create(:event) }
 
   before { login_user(user) }
 

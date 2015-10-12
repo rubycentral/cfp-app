6c78dbc2aae2c8eca9ce7375589707960179e6fa
Akira Matsuda <ronnie@dio.jp>
2015-10-12 11:15:49 +0900

    HTML first

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -4,8 +4,8 @@ class ApplicationMailer < ActionMailer::Base
   def mail_markdown(options)
     mail options do |format|
       markdown_string = render_to_string(action_name, formats: :md)
-      format.text { render text: markdown_string}
       format.html { render text: Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(markdown_string)}
+      format.text { render text: markdown_string}
     end
   end
 end

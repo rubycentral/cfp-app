844ed740a91c44bb0c6c9f4882bef43c40e3d883
Godfrey Chan <godfreykfc@gmail.com>
2015-11-18 17:02:42 -0800

    Fix CommentsController
    The unbalanced class/end, def/end means that `comment_type` etc are
    actually defined as global methods (o________________0???)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,13 @@ class CommentsController < ApplicationController
     @comment = comment_class.create(comment_params.merge(proposal: @proposal,
                                                          person: current_user))
 
+    # email all reviers and organizers about the comment
+    CommentNotificationMailer.email_notification(@comment).deliver
+
     # this action is used by the proposal show page for both speaker
     # and reviewer, so we reload the page they commented from
-      CommentNotificationMailer.email_notification(@comment).deliver
-      redirect_to :back, info: "Your comment has been added"
-    end
+    redirect_to :back, info: "Your comment has been added"
   end
-
 
   private
   def comment_type
@@ -25,4 +25,4 @@ class CommentsController < ApplicationController
   def comment_params
     params.require(comment_type.underscore).permit(:body, :proposal_id)
   end
-
+end

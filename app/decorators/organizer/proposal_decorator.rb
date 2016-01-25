ac37eedb02a997c6255498bb635f4bdbcbfc6e69
Kelly Pederson <Kelly.Pederson@gmail.com>
2016-01-25 16:42:52 -0700

    Moved rating_tooltip method to a helper method

--- app/decorators/organizer/proposal_decorator.rb
+++ app/decorators/organizer/proposal_decorator.rb
@@ -67,17 +67,6 @@ class Organizer::ProposalDecorator < ProposalDecorator
       object.state == "accepted" && object.confirmed_at == nil
   end
 
-  def rating_tooltip
-    <<-HTML
-      <p>Ratings Guide</p>
-     <p> 1 - Poor talk with many issues. Not a fit for the event.</p>
-     <p> 2 - Mediocre talk that might fit event if they work on it.</p>
-     <p> 3 - Good talk, could use improvement but appropriate for the event.</p>
-     <p> 4 - Great talk, on topic and an overall fit for the event.</p>
-     <p> 5 - Ideal talk, excellent proposal that is a perfect fit for the event.</p>
-    HTML
-  end
-
   private
 
   def state_button(text, path, opts = {})
--- app/decorators/reviewer/proposal_decorator.rb
+++ app/decorators/reviewer/proposal_decorator.rb
@@ -25,15 +25,4 @@ class Reviewer::ProposalDecorator < ProposalDecorator
   def internal_comments_style
     object.was_rated_by_person?(h.current_user) ? nil : "display: none;"
   end
-
-  def rating_tooltip
-    <<-HTML
-      <p>Ratings Guide</p>
-     <p> 1 - Poor talk with many issues. Not a fit for the event.</p>
-     <p> 2 - Mediocre talk that might fit event if they work on it.</p>
-     <p> 3 - Good talk, could use improvement but appropriate for the event.</p>
-     <p> 4 - Great talk, on topic and an overall fit for the event.</p>
-     <p> 5 - Ideal talk, excellent proposal that is a perfect fit for the event.</p>
-    HTML
-  end
 end

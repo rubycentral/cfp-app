a7c004b3a4863b10678288964cfab069f7356237
Kelly Pederson <Kelly.Pederson@gmail.com>
2016-02-02 12:02:01 -0700

    Reviewer tags are ajax call on reviewer & organizer

--- app/assets/javascripts/proposal.js
+++ app/assets/javascripts/proposal.js
@@ -19,17 +19,4 @@ $(function() {
       alert("Character limit of " + maxlength + " has been exceeded");
     }
   });
-
-  $('#tags_form').submit(function() {
-    var valuesToSubmit = $(this).serialize();
-    $.ajax({
-      type: "POST",
-      url: $(this).attr('action'), //sumbits it to the given url of the form
-      data: valuesToSubmit,
-      dataType: "JSON"
-    }).success(function(json){
-      console.log("success", json);
-    });
-    return false; // prevents normal behaviour
-  });
 });

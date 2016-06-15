8be461d9c903210f08f7a42081b307d82ced5875
Zac <zbradcow@yahoo.com>
2016-06-15 14:02:05 -0600

    added validation. squash into model commit

--- app/models/session_type.rb
+++ app/models/session_type.rb
@@ -2,6 +2,9 @@ class SessionType < ActiveRecord::Base
   belongs_to :event
   has_many :sessions
 
+  validates_presence_of :name, :event
+  validates_uniqueness_of :name, scope: :event
+
   scope :sort_by_name, ->{ order(:name) }
 end
 

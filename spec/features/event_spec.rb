0d3e44f9ce8a2c7d22ab132958d4800da3613cf6
PenneyGadget <dreamofgreen@gmail.com>
2016-06-29 11:30:26 -0600

    Modifies event CFP page layout
    Migration

--- spec/features/event_spec.rb
+++ spec/features/event_spec.rb
@@ -22,4 +22,26 @@ feature "Listing events for different roles" do
       expect(page).to have_link('1 proposal', href: event_staff_proposals_path(event))
     end
   end
+
+  context "Event CFP page" do
+    scenario "the user sees proper stats" do
+      visit event_path(event.slug)
+
+      expect(page).to have_content event.name
+      expect(page).to have_link 'Submit a proposal'
+      expect(page).to have_content "Closes at #{(DateTime.now + 21.days).strftime('%b %-d')}"
+      expect(page).to have_content '21 days left to submit your proposal'
+      expect(page).to have_content '1 proposal submitted'
+    end
+
+    scenario "the event title is a link if it's set" do
+      new_event = Event.create(name: "Coolest Event", slug: "cool", url: "", state: "open")
+      visit event_path(new_event.slug)
+
+      within('.page-header') do
+        expect(page).to have_content "Coolest Event"
+        expect(page).to_not have_link new_event.name
+      end
+    end
+  end
 end

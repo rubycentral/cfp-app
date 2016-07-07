6260fef52112bfe4ae278159150f9f50b097df75
Rylan Bowers <rylanb@gmail.com>
2016-07-06 21:01:40 -0600

    Fixing / updating event teammates spec to add skip for autocomplete email test and to test that you can invite a new teammate.

--- spec/features/staff/event_teammates_spec.rb
+++ spec/features/staff/event_teammates_spec.rb
@@ -7,19 +7,33 @@ feature "Staff Organizers can manage event_teammates" do
   before { login_as(organizer) }
 
   context "adding a new event_teammate" do
-    pending "This fails because add/invite new teammate is no longer on this page. Change path once new card is complete"
     it "autocompletes email addresses", js: true do
+      skip "Did this ever work?!"
       create(:user, email: 'harrypotter@hogwarts.edu')
       create(:user, email: 'hermionegranger@hogwarts.edu')
       create(:user, email: 'viktorkrum@durmstrang.edu')
-      visit event_staff_path(event)
+      visit event_staff_event_teammate_invitations_path(event)
 
-      click_link 'Add/Invite Staff'
-      fill_in 'email', with: 'h'
+      click_link 'Invite new event teammate'
+      fill_in 'event_teammate_invitation_email', with: 'harrypotter@hogwarts.edu'
+      select('reviewer', from: 'Role')
+      click_button 'Invite'
 
       expect(page).to have_text('harrypotter@hogwarts.edu')
       expect(page).to have_text('hermionegranger@hogwarts.edu')
       expect(page).to_not have_text('viktorkrum@durmstrang.edu')
+    end
+
+    it "invites a teammate by email addresses", js: true do
+      visit event_staff_event_teammate_invitations_path(event)
+
+      click_link 'Invite new event teammate'
+      fill_in 'event_teammate_invitation_email', with: 'harrypotter@hogwarts.edu'
+      select('reviewer', from: 'Role')
+      click_button 'Invite'
+
+      expect(page).to have_text('harrypotter@hogwarts.edu')
+      expect(page).to have_text('pending')
     end
   end
 end

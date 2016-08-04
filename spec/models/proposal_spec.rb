176953ef83d54a723912161fe03ea53ab43e7c53
R. Tyler Croy <tyler@monkeypox.org>
2016-08-04 16:09:35 -0700

    Fix the failing test, validating against our increasing maxlength
    See 2d7f87f1483a8447258f01aee54a48d0bdab10b1

--- spec/models/proposal_spec.rb
+++ spec/models/proposal_spec.rb
@@ -70,9 +70,9 @@ describe Proposal do
       expect{proposal.save}.to change{proposal.uuid}.from(nil).to('greendalec')
     end
 
-    it "limits abstracts to 600 characters or less" do
-      expect(build(:proposal, abstract: "S" * 600)).to be_valid
-      expect(build(:proposal, abstract: "S" * 601)).not_to be_valid
+    it "limits abstracts to 1000 characters or less" do
+      expect(build(:proposal, abstract: "S" * 1000)).to be_valid
+      expect(build(:proposal, abstract: "S" * 1001)).not_to be_valid
     end
   end
 

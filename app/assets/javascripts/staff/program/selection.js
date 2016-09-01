dfb99dae4b4a9cdfba1bfb3addd1cfec25efe9bd
Archana Sriram <archana.hcw@gmail.com>
2016-09-01 10:02:25 -0600

    Style updates

--- app/assets/javascripts/staff/program/selection.js
+++ app/assets/javascripts/staff/program/selection.js
@@ -1,5 +1,15 @@
 $(function() {
 
-  cfpDataTable('#organizer-proposals-selection.datatable',
-    [ 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text' ]);
+  cfpDataTable('#organizer-proposals-selection.datatable', [ 'text', 'text', 'text', 'text', 'text', 'text', 'text', 'text' ]);
+
+  $('.by-track').hide();
+
+  $('.track-select').change(function () {
+    console.log('does this work?');
+    if ($(this).val() == 'all') {
+    }
+    else {
+      $('.by-track').show();
+    }
+  });
 });

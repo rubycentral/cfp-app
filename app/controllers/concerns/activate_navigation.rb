b3898715724f24431e344fa3ea457e2386965228
Zac <zbradcow@yahoo.com>
2016-09-08 15:17:41 -0600

    More nav tweaks
    - Fixed proposal show page nav highlight issue.
    
    - Using path helpers now.
    
    - Added support for nil dependencies.
    
    - Eliminated redundancy in path mappings.

--- app/controllers/concerns/activate_navigation.rb
+++ app/controllers/concerns/activate_navigation.rb
@@ -7,43 +7,79 @@ module ActivateNavigation
     helper_method :nav_item_class
     helper_method :program_subnav_item_class
     helper_method :event_subnav_item_class
+    helper_method :schedule_subnav_item_class
   end
 
   def nav_item_class(key)
-    map = {
-      "/my-proposals" => "my-proposals-link",
-      "/events/#{current_event.slug}/staff/proposals" => "event-review-proposals-link",
-      "/events/#{current_event.slug}/staff/program/proposals/selection" => "event-program-link",
-      "/events/#{current_event.slug}/staff/time_slots" => "event-schedule-link",
-      "/events/#{current_event.slug}/staff" => "event-dashboard-link",
-      "/events/#{current_event.slug}/staff/info" => "event-dashboard-link",
-      "/events/#{current_event.slug}/staff/team" => "event-dashboard-link",
-      "/events/#{current_event.slug}/staff/config" => "event-dashboard-link",
-      "/events/#{current_event.slug}/staff/guidelines" => "event-dashboard-link",
-      "/events/#{current_event.slug}/staff/speaker-emails" => "event-dashboard-link",
-    }
-    return "active" if key == map[request.path]
+    return 'active' if matches_nav_path?(key, nav_item_map)
   end
 
   def event_subnav_item_class(key)
-    map = {
-      "/events/#{current_event.slug}/staff" => "event-staff-dashboard-link",
-      "/events/#{current_event.slug}/staff/info" => "event-staff-info-link",
-      "/events/#{current_event.slug}/staff/team" => "event-staff-teammates-link",
-      "/events/#{current_event.slug}/staff/config" => "event-staff-config-link",
-      "/events/#{current_event.slug}/staff/guidelines" => "event-staff-guidelines-link",
-      "/events/#{current_event.slug}/staff/speaker-emails" => "event-staff-speaker-emails-link",
-    }
-    return "active" if key == map[request.path]
+    return 'active' if matches_nav_path?(key, event_subnav_item_map)
   end
 
   def program_subnav_item_class(key)
-    map = {
-      "/events/#{current_event.slug}/staff/program/proposals/selection" => "event-program-proposals-selection-link",
-      "/events/#{current_event.slug}/staff/program/proposals" => "event-program-proposals-link",
-      "/events/#{current_event.slug}/staff/program/sessions" => "event-program-sessions-link",
-      "/events/#{current_event.slug}/staff/program/speakers" => "event-program-speakers-link",
+    return 'active' if matches_nav_path?(key, program_subnav_item_map)
+  end
+
+  def schedule_subnav_item_class(key)
+    {} #TBD
+  end
+
+  def matches_nav_path?(key, paths={})
+    if paths.is_a?(String)
+      return paths==request.path
+
+    elsif paths.is_a?(Hash)
+      return matches_nav_path?(nil, paths.values) if key.nil?
+      return paths.any?{|k, p| k==key && matches_nav_path?(nil, p) } # only recurse if the key matches
+
+    elsif paths.is_a?(Array) || key.nil?
+      return paths.any?{|p| !p.nil? && matches_nav_path?(nil, p) }
+    end
+  end
+
+  def nav_item_map
+    @nav_item_map ||= {
+        'my-proposals-link' => add_path(:proposals),
+        'event-review-proposals-link' => [
+            add_path(:event_staff_proposals, current_event),
+            add_path(:event_staff_proposal, current_event, @proposal)
+        ],
+        'event-program-link' => program_subnav_item_map.values,
+        'event-schedule-link' => add_path(:event_staff_time_slots, current_event),
+        'event-dashboard-link' => event_subnav_item_map.values,
     }
-    return "active" if key == map[request.path]
+  end
+
+  def event_subnav_item_map
+    @event_subnav_item_map ||= {
+        'event-staff-dashboard-link' => add_path(:event_staff, current_event),
+        'event-staff-info-link' => add_path(:event_staff_info, current_event),
+        'event-staff-teammates-link' => add_path(:event_staff_teammates, current_event),
+        'event-staff-config-link' => add_path(:event_staff_config, current_event),
+        'event-staff-guidelines-link' => add_path(:event_staff_guidelines, current_event),
+        'event-staff-speaker-emails-link' => add_path(:event_staff_speaker_email_notifications, current_event),
+    }
+  end
+
+  def program_subnav_item_map
+    @program_subnav_item_map ||= {
+        'event-program-proposals-selection-link' => [
+            add_path(:selection_event_staff_program_proposals, current_event),
+            # add_path(:event_staff_program_proposal, current_event, @proposal)
+            #How to leverage session[:prev_page] here ?
+        ],
+        'event-program-proposals-link' => [
+            add_path(:event_staff_program_proposals, current_event),
+            add_path(:event_staff_program_proposal, current_event, @proposal)
+        ],
+        'event-program-sessions-link' => add_path(:event_staff_program_sessions, current_event),
+        'event-program-speakers-link' => add_path(:event_staff_program_speakers, current_event),
+    }
+  end
+
+  def add_path(sym, *deps)
+    send(sym.to_s + '_path', *deps) unless deps.include?(nil) # don't generate the path unless all dependencies are present
   end
 end

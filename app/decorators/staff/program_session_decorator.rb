d48a48b411b9bc1363878c4cb6958367c2ac811d
Zac <zbradcow@yahoo.com>
2016-10-04 10:28:02 -0600

    Fixed issue with confirmation note popover icon being on the wrong page.

--- app/decorators/staff/program_session_decorator.rb
+++ app/decorators/staff/program_session_decorator.rb
@@ -27,6 +27,15 @@ class Staff::ProgramSessionDecorator < ApplicationDecorator
     h.content_tag :span, state, class: classes
   end
 
+  def confirmation_notes_link
+    return '' unless object.confirmation_notes?
+    id = h.dom_id(object, 'notes')
+    h.link_to '#', id: id, title: 'Confirmation notes', class: 'popover-trigger', role: 'button', tabindex: 0, data: {
+        toggle: 'popover', content: object.confirmation_notes, target: "##{id}", placement: 'bottom', trigger: 'manual'} do
+      h.content_tag(:i, '', class: 'fa fa-file')
+    end
+  end
+
   def state_class(state)
     case state
     when ProgramSession::ACTIVE

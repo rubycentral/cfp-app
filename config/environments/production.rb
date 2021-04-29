2eb65c2e2c9de15497603afa3bacc937a1e6f4c8
Evan Phoenix <evan@phx.io>
2021-04-29 23:40:01 +0000

    Allow SENDGRID_API_KEY to not set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
     port: ENV.fetch('SMTP_PORT', '587'),
     authentication: :plain,
     user_name: "apikey",
-    password: ENV.fetch('SENDGRID_API_KEY'),
+    password: ENV.fetch('SENDGRID_API_KEY', ""),
     domain: 'heroku.com',
     enable_starttls_auto: true
   }

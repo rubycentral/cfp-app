6db63e98d2aee1c41b3712bad203277de188c584
Cody Brooks <codyjamesbrooks@gmail.com>
2022-04-09 19:28:09 -0600

    initial WIP on programs page

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,9 +1,8 @@
 class ProgramsController < ApplicationController
   before_action :require_event
- 
+
   def index
-    @program_session = current_event.program_sessions
+    @program_sessions = current_event.program_sessions.live
     render layout: "themes/#{current_website.theme}"
   end
 end
- 

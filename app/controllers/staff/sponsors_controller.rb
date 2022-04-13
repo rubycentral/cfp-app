81fdfca4f01f83844f05185aff3447cbb2c01d6c
Cody Brooks <codyjamesbrooks@gmail.com>
2022-04-13 16:28:38 -0600

    event sponsor CRUD
    This is most of the CRUD for the sponsors table. (all but :delete)
    I beleve it follows most of the style conventions that are used throughout the staff portion of CFP
    By that I mean
    - leverages simple_form through the _form partial for new, and edit
    - Index displays the collection by rendering the _sponsor partial
    
    TODO's
    - Will need to do some tests
    - Will need to work out the business rules around Sponsor creation
    - Images, I held off on :logos, becuase I think JG was working on image upload today.

--- app/controllers/staff/sponsors_controller.rb
+++ app/controllers/staff/sponsors_controller.rb
@@ -5,15 +5,25 @@ class Staff::SponsorsController < Staff::ApplicationController
   end
 
   def new
+    @sponsor = current_event.sponsors.build
   end
 
   def create
+    @sponsor = current_event.sponsors.build(sponsors_params)
+
+    flash[:success] = "Sponsor was successfully created."
+    redirect_to event_staff_sponsors_path if @sponsor.save
   end
 
   def edit
+    @sponsor = current_event.sponsors.find_by(id: params[:id])
   end
 
   def update
+    @sponsor = current_event.sponsors.find_by(id: params[:id])
+    @sponsor.update(sponsors_params)
+    flash[:success] = "#{@sponsor.name} was successfully updated."
+    redirect_to event_staff_sponsors_path
   end
 
   private

b019267eb7a2e2030747ed25ad1bf81ce3c6fb5e
Cody Brooks <codyjamesbrooks@gmail.com>
2022-04-17 17:17:03 -0600

    use different naming for schedule sessions
    moved away from session, for obvious reasons

--- app/helpers/website_schedule_helper.rb
+++ app/helpers/website_schedule_helper.rb
@@ -1,9 +1,9 @@
 module WebsiteScheduleHelper
 
   def schedule_for_day(schedule, day_number)
-    schedule.select { |session| session[:conference_day] == day_number }
-            .sort_by {|session| session[:start_time] }
-            .group_by {|session| session[:start_time] }
+    schedule.select { |program_session| program_session[:conference_day] == day_number }
+            .sort_by {|program_session| program_session[:start_time] }
+            .group_by {|program_session| program_session[:start_time] }
             .values
   end
 end

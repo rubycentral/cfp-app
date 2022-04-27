d4603915f544c89a174fbd47deaaef2016ae56a8
Cody Brooks <codyjamesbrooks@gmail.com>
2022-04-27 12:31:26 -0600

    feature testing website sponsors happy path
    Tests the following.
    - Unpublished sponsors do not appear on the sponsors page
    - Sponsors page updates when a sponsor is updated
    - Deleted sponsors are removed from the sponsor page
    - Including stimulus sponsor-footer controller in a page body creates a sponsor-footer component on that page
    - Sponsor footer displays a sponsors footer logo, not its primary logo
    - If the sponsor has an offer, a user can click to reveal the offer
    
    Additional note
    - Added made some small syntax revisions per johnsgreen PR comments.

--- spec/factories/sponsors.rb
+++ spec/factories/sponsors.rb
@@ -2,5 +2,22 @@ FactoryBot.define do
   factory :sponsor do
     event
     name { Faker::Company.name }
+    published { true }
+    tier { 'platinum' }
+
+    description { Faker::Hipster.paragraph }
+    primary_logo_path = Rails.root.join('spec/fixtures/files/ruby1.png')
+    primary_logo { Rack::Test::UploadedFile.new primary_logo_path, "image/png" }
+
+    trait :with_footer_logo do
+      footer_logo_path = Rails.root.join('spec/fixtures/files/ruby2.jpeg')
+      footer_logo { Rack::Test::UploadedFile.new footer_logo_path, "image/jpeg" }
+    end
+
+    trait :with_offer do
+      offer_headline { "A really great offer" }
+      offer_url { "http://fakeoffer.com/" }
+      offer_text { Faker::Hipster.paragraph }
+    end
   end
 end

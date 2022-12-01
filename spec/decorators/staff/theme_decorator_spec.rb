96991881eed0f31af580279cf5f75ad22ae27816
Devon St.Clair <devonstclair@gmail.com>
2022-11-30 16:58:27 -0800

    Add additional theme layouts for events
    * Add V1 of the about template
    * Add routing to preview theme pages outside of website builder
    * Make theme pages only available after auth

---
+++ spec/decorators/staff/theme_decorator_spec.rb
@@ -0,0 +1,4 @@
+require 'rails_helper'
+
+RSpec.describe Staff::ThemeDecorator do
+end
---
+++ spec/requests/staff/themes_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Staff::Themes", type: :request do
+  describe "GET /index" do
+    pending "add some examples (or delete) #{__FILE__}"
+  end
+end

98a449cf23a88db6c409443a472bd31df5db66bb
Akira Matsuda <ronnie@dio.jp>
2024-11-14 19:44:17 +0900

    Avoid using hashrocket for a kwarg
    The hashrocket here will be treated as a Hash object literal rather than
    a kwarg under recent versions of Ruby, and this will end up with
    creating broken schema.rb with the following diff via db:migrate
    -    t.string "item_type", null: false
    +    t.string "item_type"
    +    t.string "{:null=>false}"

--- db/migrate/20200223215131_create_versions.rb
+++ db/migrate/20200223215131_create_versions.rb
@@ -10,7 +10,7 @@ class CreateVersions < ActiveRecord::Migration[5.2]
 
   def change
     create_table :versions do |t|
-      t.string   :item_type, {:null=>false}
+      t.string   :item_type, null: false
       t.integer  :item_id,   null: false, limit: 8
       t.string   :event,     null: false
       t.string   :whodunnit

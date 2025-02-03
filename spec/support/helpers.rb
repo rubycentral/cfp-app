64a6010b169efdc2d8883b3163f5390172a700f5
Akira Matsuda <ronnie@dio.jp>
2025-02-04 03:12:57 +0900

    include helpers in system specs

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -2,6 +2,6 @@ require 'support/helpers/session_helpers'
 require 'support/helpers/form_helpers'
 
 RSpec.configure do |config|
-  config.include Features::SessionHelpers, type: :feature
-  config.include Features::FormHelpers, type: :feature
+  config.include Features::SessionHelpers, type: :system
+  config.include Features::FormHelpers, type: :system
 end

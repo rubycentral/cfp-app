ad6ac1f40282cdb4f708d847914ffea0bde13a4d
Akira Matsuda <ronnie@dio.jp>
2025-02-15 19:34:29 +0900

    Load Rails 6.0 defaults
    self.autoloader = :zeitwerk if RUBY_ENGINE == "ruby"
    
    action_view.default_enforce_utf8 = false
    
    action_dispatch.use_cookies_with_metadata = true
    
    action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"
    
    active_storage.queues.analysis = :active_storage_analysis
    active_storage.queues.purge    = :active_storage_purge
    
    active_storage.replace_on_assign_to_many = true
    
    active_record.collection_cache_versioning = true

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(*Rails.groups)
 module CFPApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Configuration for the application, engines, and railties goes here.
     #

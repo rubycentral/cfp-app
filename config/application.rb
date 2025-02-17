e570e39fba43536b62ab31efc2ff3a7302193a38
Akira Matsuda <ronnie@dio.jp>
2025-02-18 02:45:55 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,20 @@
 require_relative "boot"
 
-# Working around on missing logger problem by requiring 'logger' earlier than 'rails'...
-require "logger"
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CFPApp
+module CfpApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
     #

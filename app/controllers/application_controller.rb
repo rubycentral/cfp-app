a8c5b4be99a2ea891e9dd317f2a72b02099d1cc0
Akira Matsuda <ronnie@dio.jp>
2025-02-18 12:33:34 +0900

    Don't `require` in application code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   include ActivateNavigation
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-  require "csv"
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception

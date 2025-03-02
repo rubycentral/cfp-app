e728d4dd3a664c8b458163c53740b3949dc5c8c0
Akira Matsuda <ronnie@dio.jp>
2025-03-03 03:58:43 +0900

    s/DateTime.now/Time.now/g

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -103,7 +103,7 @@ describe Notification do
 
   describe "#mark_as_read" do
     let(:user) { create(:user) }
-    it "sets read_at to DateTime.now" do
+    it "sets read_at to Time.now" do
       now = Time.current
       allow(DateTime).to receive(:current) { now }
       notification = create(:notification, user: user)

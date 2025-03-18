77610bbc1eaeeca8cfabe997b0805733c129168d
Akira Matsuda <ronnie@dio.jp>
2025-03-18 19:03:22 +0900

    Count ratings with SQL

--- app/controllers/staff/proposal_reviews_controller.rb
+++ app/controllers/staff/proposal_reviews_controller.rb
@@ -13,10 +13,11 @@ class Staff::ProposalReviewsController < Staff::ApplicationController
     proposals = policy_scope(Proposal)
       .select("*,
         (#{Rating.select('score').where('proposal_id = proposals.id').where(user_id: current_user).to_sql}) as your_score,
+        (#{Rating.select('COUNT(*)').where('proposal_id = proposals.id').to_sql}) as ratings_count,
         (#{Comment.select('COUNT(*)').where('proposal_id = proposals.id').to_sql}) as comments_count")
       .includes(:proposal_taggings, :review_taggings, :ratings, :session_format)
 
-    proposals.to_a.sort_by! { |p| [p.ratings.present? ? 1 : 0, p.created_at] }
+    proposals.to_a.sort_by! { |p| [p.ratings_count > 0 ? 1 : 0, p.created_at] }
     proposals = Staff::ProposalDecorator.decorate_collection(proposals)
 
     render locals: {

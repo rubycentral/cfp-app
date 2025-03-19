7e8a9d44cb6a1e5c96ea312ae25dd7a41b680217
Akira Matsuda <ronnie@dio.jp>
2025-03-20 02:24:59 +0900

    Count rated proposals in all tracks at once

--- app/models/event_stats.rb
+++ app/models/event_stats.rb
@@ -52,11 +52,14 @@ class EventStats
     stats = {'Total' => track_review_stats}
 
     proposals_per_track = event.proposals.left_joins(:track).group('tracks.name').count
+    rated_proposals_per_track = event.proposals.rated.left_joins(:track).group('tracks.name').count
     stats['Total'][:proposals] = proposals_per_track.values.sum || 0
+    stats['Total'][:reviews] = rated_proposals_per_track.values.sum || 0
 
     event.tracks.each do |track|
       stats[track.name] = track_review_stats(track.id)
       stats[track.name][:proposals] = proposals_per_track[track.name] || 0
+      stats[track.name][:reviews] = rated_proposals_per_track[track.name] || 0
     end
     stats
   end
@@ -68,7 +71,6 @@ class EventStats
     comments_per_type = Comment.joins(:proposal).group(:type).where(proposal: p).count
 
     {
-      reviews: p.rated.count,
       public_comments: comments_per_type['PublicComment'] || 0,
       internal_comments: comments_per_type['InternalComment'] || 0,
       needs_review: p.left_outer_joins(:ratings).group("proposals.id").having("count(ratings.id) < ?", 2).length

26be894d29364bd0b381aa88f9f694bbe17025ae
Akira Matsuda <ronnie@dio.jp>
2025-11-26 12:31:55 +0900

    Use HTML5 date_field for inputting date

--- app/assets/javascripts/admin/events.js
+++ app/assets/javascripts/admin/events.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  $('#event_closes_at, #event_opens_at, #event_start_date, #event_end_date').datetimepicker();
+  $('#event_closes_at, #event_opens_at').datetimepicker();
 
   $('.dataTables_info').addClass('text-muted');
 });

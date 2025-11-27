8bfe0138ea53a1ee3dff460bab4ef815f172f8df
Akira Matsuda <ronnie@dio.jp>
2025-11-28 08:16:45 +0900

    Import DataTables + Bootstrap 3 styling

---
+++ app/javascript/datatables.js
@@ -0,0 +1,7 @@
+// Import DataTables and Bootstrap 3 styling
+// jQuery is provided as external from Sprockets (jquery-rails gem)
+import 'datatables.net'
+import 'datatables.net-bs'
+
+// Import DataTables Bootstrap 3 CSS
+import 'datatables.net-bs/css/dataTables.bootstrap.css'

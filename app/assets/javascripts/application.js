8a00098e435d74ce61e6be22910567a275f0e7b4
Akira Matsuda <ronnie@dio.jp>
2025-11-29 12:09:29 +0900

    We're no longer using underscore in our app

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,6 @@
 // about supported directives.
 //
 //= require jquery/dist/jquery
-//= require underscore/underscore
 //= require @rails/ujs/app/assets/javascripts/rails-ujs
 //= require bootstrap
 //= require chartkick

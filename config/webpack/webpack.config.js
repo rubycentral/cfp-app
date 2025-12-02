ceebc05842fdd40bd1873287b388ce2cb263e24e
Akira Matsuda <ronnie@dio.jp>
2025-12-03 04:02:35 +0900

    Skip running babel-loader

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,5 +1,22 @@
 const { webpackConfig, merge } = require('shakapacker')
 
+// Remove babel-loader - modern browsers support ES6+ natively
+webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
+  if (rule.use && Array.isArray(rule.use)) {
+    rule.use = rule.use.filter(loader => {
+      const loaderName = typeof loader === 'string' ? loader : loader.loader
+      return !loaderName || !loaderName.includes('babel-loader')
+    })
+  }
+  return rule
+}).filter(rule => {
+  // Remove rules that only had babel-loader
+  if (rule.use && Array.isArray(rule.use) && rule.use.length === 0) {
+    return false
+  }
+  return true
+})
+
 const customConfig = {
   resolve: {
     extensions: [
@@ -17,12 +34,7 @@ const customConfig = {
   externals: {
     // Use jQuery from Sprockets (loaded via node_modules) as external
     jquery: 'jQuery'
-  },
-  ignoreWarnings: [
-    {
-      module: /tailwind\.js/
-    },
-  ]
+  }
 }
 
 module.exports = merge(webpackConfig, customConfig)

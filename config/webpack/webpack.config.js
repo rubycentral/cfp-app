e21d8f78660389545b069d25265fb5dcdd99da2e
Akira Matsuda <ronnie@dio.jp>
2025-12-03 04:26:14 +0900

    Use esbuild instead of babel

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,22 +1,5 @@
 const { webpackConfig, merge } = require('shakapacker')
 
-// Remove babel-loader - modern browsers support ES6+ natively
-webpackConfig.module.rules = webpackConfig.module.rules.map(rule => {
-  if (rule.use && Array.isArray(rule.use)) {
-    rule.use = rule.use.filter(loader => {
-      const loaderName = typeof loader === 'string' ? loader : loader.loader
-      return !loaderName || !loaderName.includes('babel-loader')
-    })
-  }
-  return rule
-}).filter(rule => {
-  // Remove rules that only had babel-loader
-  if (rule.use && Array.isArray(rule.use) && rule.use.length === 0) {
-    return false
-  }
-  return true
-})
-
 const customConfig = {
   resolve: {
     extensions: [

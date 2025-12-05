8474104320ae60e7d64ba2b2f21c1c9275f89746
Akira Matsuda <ronnie@dio.jp>
2025-12-05 13:28:39 +0900

    Inline notes_tooltip that is referenced only once

--- app/helpers/proposal_helper.rb
+++ app/helpers/proposal_helper.rb
@@ -38,8 +38,4 @@ module ProposalHelper
       end
     end
   end
-
-  def notes_tooltip
-    "Please note any scheduling conflicts, or any additional information an organizer may need to schedule your talk."
-  end
 end

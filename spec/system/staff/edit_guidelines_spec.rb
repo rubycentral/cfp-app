9972ec99eea3c9d4d5795d768d4a8dd35e15fd58
Akira Matsuda <ronnie@dio.jp>
2025-12-06 04:42:51 +0900

    Adjust specs

--- spec/system/staff/edit_guidelines_spec.rb
+++ spec/system/staff/edit_guidelines_spec.rb
@@ -37,20 +37,20 @@ feature "Event Guidelines", type: :system do
     it "can edit event guidelines", js: true do
       visit event_staff_guidelines_path(event)
 
-      expect(page).to have_button "Edit Guidelines"
+      expect(page).to have_link "Edit Guidelines"
       expect(page).to have_content "We want all the good talks!"
       expect(page).to_not have_button "Save Guidelines"
       expect(page).to_not have_link "Cancel"
 
       click_on "Edit Guidelines"
-      fill_in "text-box", with: "New Guidelines!"
+      fill_in "event_guidelines", with: "New Guidelines!"
       click_on "Save Guidelines"
 
       expect(page).to have_content "New Guidelines!"
-      expect(page).to have_button "Edit Guidelines"
+      expect(page).to have_link "Edit Guidelines"
 
       click_on "Edit Guidelines"
-      fill_in "text-box", with: "BLARG"
+      fill_in "event_guidelines", with: "BLARG"
       click_on "Cancel"
 
       expect(page).to have_content "New Guidelines!"
@@ -63,20 +63,20 @@ feature "Event Guidelines", type: :system do
     it "can edit event guidelines", js: true do
       visit event_staff_guidelines_path(event)
 
-      expect(page).to have_button "Edit Guidelines"
+      expect(page).to have_link "Edit Guidelines"
       expect(page).to have_content "We want all the good talks!"
       expect(page).to_not have_button "Save Guidelines"
       expect(page).to_not have_link "Cancel"
 
       click_on "Edit Guidelines"
-      fill_in "text-box", with: "New Guidelines!"
+      fill_in "event_guidelines", with: "New Guidelines!"
       click_on "Save Guidelines"
 
       expect(page).to have_content "New Guidelines!"
-      expect(page).to have_button "Edit Guidelines"
+      expect(page).to have_link "Edit Guidelines"
 
       click_on "Edit Guidelines"
-      fill_in "text-box", with: "BLARG"
+      fill_in "event_guidelines", with: "BLARG"
       click_on "Cancel"
 
       expect(page).to have_content "New Guidelines!"
@@ -89,7 +89,7 @@ feature "Event Guidelines", type: :system do
     it "cannot edit event guidelines", js: true do
       visit event_staff_guidelines_path(event)
 
-      expect(page).to_not have_button "Edit Guidelines"
+      expect(page).to_not have_link "Edit Guidelines"
       expect(page).to have_content "We want all the good talks!"
     end
   end
@@ -100,7 +100,7 @@ feature "Event Guidelines", type: :system do
     it "cannot edit event guidelines", js: true do
       visit event_staff_guidelines_path(event)
 
-      expect(page).to_not have_button "Edit Guidelines"
+      expect(page).to_not have_link "Edit Guidelines"
       expect(page).to have_content "We want all the good talks!"
     end
   end

7bb13422be181a8666c4f4a86b4ea4c33d9cec4d
Akira Matsuda <ronnie@dio.jp>
2025-12-06 08:19:17 +0900

    Set tribute.js `@mentions` data via Stimulus

---
+++ app/javascript/controllers/mention_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from '@hotwired/stimulus'
+
+export default class extends Controller {
+  connect() {
+    const mentionNames = JSON.parse(this.element.dataset.mentionNames || '[]') || []
+    const values = mentionNames.filter(Boolean).map(name => ({ key: name, value: name }))
+
+    const tribute = new Tribute({
+      trigger: '@',
+      values: values,
+      selectTemplate: item => '@' + item.original.value,
+      menuItemTemplate: item => '@' + item.original.value,
+      noMatchTemplate: '',
+      lookup: 'key',
+      fillAttr: 'value'
+    })
+
+    tribute.attach(this.element)
+  }
+}

323a85c36273b79059d4f9089c7afdd87f896255
Akira Matsuda <ronnie@dio.jp>
2025-12-10 22:40:34 +0900

    Exclude rails-ujs from the assets
    because this app relies on Turbo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,3 +3,8 @@
 # Note: node_modules is NOT added to asset paths.
 # JS is bundled by esbuild (jsbundling-rails) to app/assets/builds/
 # CSS is bundled by postcss (cssbundling-rails) to app/assets/builds/
+
+# Exclude rails-ujs from actionview since we use Turbo instead
+Rails.application.config.after_initialize do
+  Rails.application.config.assets.paths.reject! { |p| p.to_s.include?('actionview') }
+end

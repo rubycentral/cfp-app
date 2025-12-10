06ab64f6be1f274f22201e892c15dbd21cb442b5
Akira Matsuda <ronnie@dio.jp>
2025-12-10 13:22:44 +0900

    The main application.js entry point

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,9 @@
 // Entry point for the build script in your package.json
+
+import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+
+// Disable Turbo Drive globally - opt-in instead of opt-out for existing app
+Turbo.session.drive = false
+
+import "./controllers"

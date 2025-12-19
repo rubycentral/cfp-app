37434ce0d5429b95e94cf1dffd6742a8dc583075
Akira Matsuda <ronnie@dio.jp>
2025-12-19 14:54:47 +0900

    Numbers of submitted/accepted proposals for admin/users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < Admin::ApplicationController
 
   # GET /admin/users
   def index
-    render locals: { users: User.includes(:teammates) }
+    render locals: { users: User.includes(:teammates, :proposals) }
   end
 
   # GET /admin/users/1

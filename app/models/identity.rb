045ceb1f2a44244bcf04c7383f7610421afd3eea
Akira Matsuda <ronnie@dio.jp>
2025-12-19 15:21:24 +0900

    Identity.validates_presence_of provider and uid

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,5 +1,8 @@
 class Identity < ApplicationRecord
   belongs_to :user
+
+  validates :provider, presence: true
+  validates :uid, presence: true, uniqueness: {scope: :provider}
 end
 
 # == Schema Information

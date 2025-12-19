09fc084b4ef328ee5b845583449c048576100fe7
Akira Matsuda <ronnie@dio.jp>
2025-12-19 12:17:33 +0900

    Show currently hidden content on a browser native tooltip

--- app/javascript/controllers/card_display_toggle_controller.js
+++ app/javascript/controllers/card_display_toggle_controller.js
@@ -5,11 +5,20 @@ export default class extends Controller {
 
   connect() {
     this.showingSpeakers = false
+    this.updateTooltips()
   }
 
   toggle() {
     this.showingSpeakers = !this.showingSpeakers
     this.element.classList.toggle('show-speakers', this.showingSpeakers)
     this.buttonTarget.textContent = this.showingSpeakers ? 'Show Titles' : 'Show Speakers'
+    this.updateTooltips()
+  }
+
+  updateTooltips() {
+    const cards = this.element.querySelectorAll('.draggable-session-card')
+    cards.forEach(card => {
+      card.title = this.showingSpeakers ? card.dataset.title : card.dataset.speakers
+    })
   }
 }

12323b57c91768ec10674895a87ff51eefa0a61f
Akira Matsuda <ronnie@dio.jp>
2025-12-19 15:17:33 +0900

    rails g model Identity user:references provider:string uid:string

---
+++ app/models/identity.rb
@@ -0,0 +1,20 @@
+class Identity < ApplicationRecord
+  belongs_to :user
+end
+
+# == Schema Information
+#
+# Table name: identities
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  provider   :string           not null
+#  uid        :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_identities_on_provider_and_uid  (provider,uid) UNIQUE
+#  index_identities_on_user_id           (user_id)
+#

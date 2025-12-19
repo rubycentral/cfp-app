762f1fa83e1009670115730c48754ae21e18f48e
Akira Matsuda <ronnie@dio.jp>
2025-12-19 12:24:50 +0900

    CSS tooltip that shows quickly instead of the browser native title

--- app/javascript/controllers/card_display_toggle_controller.js
+++ app/javascript/controllers/card_display_toggle_controller.js
@@ -3,22 +3,9 @@ import { Controller } from '@hotwired/stimulus'
 export default class extends Controller {
   static targets = ['button']
 
-  connect() {
-    this.showingSpeakers = false
-    this.updateTooltips()
-  }
-
   toggle() {
-    this.showingSpeakers = !this.showingSpeakers
-    this.element.classList.toggle('show-speakers', this.showingSpeakers)
-    this.buttonTarget.textContent = this.showingSpeakers ? 'Show Titles' : 'Show Speakers'
-    this.updateTooltips()
-  }
-
-  updateTooltips() {
-    const cards = this.element.querySelectorAll('.draggable-session-card')
-    cards.forEach(card => {
-      card.title = this.showingSpeakers ? card.dataset.title : card.dataset.speakers
-    })
+    this.element.classList.toggle('show-speakers')
+    const showingSpeakers = this.element.classList.contains('show-speakers')
+    this.buttonTarget.textContent = showingSpeakers ? 'Show Titles' : 'Show Speakers'
   }
 }

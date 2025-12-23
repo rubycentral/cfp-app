fe73ae8e026923380cd460f985b218c95fe5b669
Akira Matsuda <ronnie@dio.jp>
2025-12-23 19:38:30 +0900

    Configure :headless_chrome driver with options for stability

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -98,8 +98,18 @@ RSpec.configure do |config|
     driven_by :rack_test
   end
 
+  Capybara.register_driver :headless_chrome do |app|
+    options = Selenium::WebDriver::Chrome::Options.new
+    options.add_argument('--headless=new')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1400,1400')
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+  end
+
   config.before type: :system, js: true do
-    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+    driven_by :headless_chrome
   end
 
   Capybara.disable_animation = true

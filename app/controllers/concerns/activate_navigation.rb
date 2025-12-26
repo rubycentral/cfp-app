35f484530db910d169e06da4b2d4c2c0d8690c21
Akira Matsuda <ronnie@dio.jp>
2025-12-26 19:01:58 +0900

    Guard properly
    Currently, if no nav matches, both `@active_nav_key` and `@active_subnav_key`
    stay nil, and nil || nil is falsy, so we recalculate on every call.

--- app/controllers/concerns/activate_navigation.rb
+++ app/controllers/concerns/activate_navigation.rb
@@ -21,7 +21,7 @@ module ActivateNavigation
   private
 
   def initialize_nav
-    return if @active_nav_key || @active_subnav_key
+    return if defined?(@active_nav_key)
 
     @active_nav_key, subnav_map = find_first(nav_item_map)
     @active_subnav_key, _ = find_first(subnav_map)

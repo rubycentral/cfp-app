f9c8a4a964d84bee8d4e8439a8217ba24ae33cc0
Akira Matsuda <ronnie@dio.jp>
2026-01-06 19:22:19 +0900

    Expand rails/all

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,16 @@
 require_relative "boot"
 
-require "rails/all"
+require 'active_record/railtie'
+require 'active_storage/engine'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+require 'active_job/railtie'
+require 'action_cable/engine'
+require 'action_mailbox/engine'
+require 'action_text/engine'
+require 'rails/test_unit/railtie'
+
 require 'digest/sha1'
 require 'digest/md5'
 
